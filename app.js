4740219ba96bb1e365a08554c7003860c14eda86
CodrutMH <codrutmihalca@gmail.com>
2023-08-11 18:50:24 +0300

    added buttons and functionality to them

--- app.js
+++ app.js
@@ -1,11 +1,54 @@
-const parent = document.querySelector('.square-container');
+const gridContainer = document.querySelector('.grid-container');
+
+//buttons
+const normalModBtn = document.getElementById('normal-mod');
+const rainbowModBtn = document.getElementById('rainbow-mod');
+const eraser = document.getElementById('eraser');
+const clearModBtn = document.getElementById('clear-mod');
+
+let slider = document.getElementById('myRange');
+let output = document.getElementById('sizeValue');
+
+slider.oninput = function () {
+  output.innerHTML = `${this.value} x ${this.value}`;
+  generateDivs(this.value);
+};
+// output.innerHTML = slider.value;
+generateDivs(16);
 
 function generateDivs(divs) {
-  for (let i = 0; i < divs; i++) {
+  gridContainer.innerHTML = '';
+  gridContainer.style.gridTemplateColumns = `repeat(${divs}, 1fr)`;
+
+  for (let i = 0; i < divs * divs; i++) {
     const newDiv = document.createElement('div');
     newDiv.setAttribute('class', 'grid-item');
-    parent.appendChild(newDiv);
+    gridContainer.appendChild(newDiv);
+
+    newDiv.addEventListener('mouseover', () => {
+      newDiv.style.backgroundColor = 'red'; // Change color to red on hover
+    });
+
+    //   newDiv.addEventListener('mouseout', () => {
+    //     newDiv.style.backgroundColor = 'transparent'; // Reset color on mouseout
+    //   });
+    // }
   }
 }
 
-generateDivs(16);
+clearModBtn.addEventListener('click', () => {
+  const gridItems = document.querySelectorAll('.grid-item');
+
+  gridItems.forEach((gridItem) => {
+    gridItem.style.backgroundColor = 'white'; // Clear the background color
+  });
+});
+
+eraser.addEventListener('click', () => {
+  const gridItems = document.querySelectorAll('.grid-item');
+  gridItems.forEach((gridItem) => {
+    gridItem.addEventListener('mouseover', () => {
+      gridItem.style.backgroundColor = 'white'; // Change color to red on hover
+    });
+  });
+});
